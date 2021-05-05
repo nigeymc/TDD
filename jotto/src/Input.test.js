3720ea0c7504d1f477918626ea14c5ab69d5a68f
nigeymc <mckenna.niall@gmail.com>
2021-05-05 15:03:22 +0100

    mock event added for controlled input

--- jotto/src/Input.test.js
+++ jotto/src/Input.test.js
@@ -1,24 +1,35 @@
+import React from 'react';
 import { shallow } from 'enzyme';
 
 import { findByTestAttr, checkProps } from '../test/testUtils';
 import Input from './Input';
 
-const defaultProps = {
-
+const setup = (secretWord = 'party') => {
+    return shallow(<Input secretWord={secretWord} />)
 };
 
-const setup = (props = {}) => {
-    const setupProps = {};
-    return shallow(<Input {...setupProps} />)
-};
+test('renders without error', () => {
+    const wrapper = setup();
+    const inputComponent = findByTestAttr(wrapper, 'component-input');
+    expect(inputComponent.length).toBe(1);
+});
 
-describe('renders without error', () => {
-    let wrapper;
-    beforeEach(() => {
-        wrapper = setup({ guessedWords: [] });
-    });
-    test('renders without error', () => {
-        const inputComponent = findByTestAttr(wrapper, 'component-input');
-        expect(inputComponent.length).toBe(1);
+
+test('does not throw warning with expected props', () => {
+    checkProps(Input, { secretWord: [] });
+});
+
+describe('state controlled input field', () => {
+    test('state updates with value of input box upon change', () => {
+        const mockSetCurrentGuess = jest.fn();
+        React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
+
+        const wrapper = setup();
+        const inputBox = findByTestAttr(wrapper, 'input-box');
+
+        const mockEvent = { target: { value: 'train' } };
+        inputBox.simulate('change', mockEvent);
+
+        expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
     });
 });
